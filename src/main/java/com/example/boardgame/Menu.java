16649ddd969871b2d21f9aa30fdafe77163c4b2f
Madness-Incorp <oisinmlynch04@gmail.com>
2024-01-27 17:06:47 +0000

    Check to see if menu opens

--- src/main/java/com/example/boardgame/Menu.java
+++ src/main/java/com/example/boardgame/Menu.java
@@ -8,23 +8,24 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+    public class Menu extends Application {
+        public Menu() {
+        }
 
+        public void start(Stage stage) throws IOException {
+            Parent root = (Parent) FXMLLoader.load(this.getClass().getResource("menuScreen.fxml"));
+            Scene scene = new Scene(root);
+            root.getStylesheets().add(this.getClass().getResource("Scene.css").toExternalForm());
+            Image icon = new Image(this.getClass().getResource("GameIcon.jpeg").toExternalForm());
+            stage.getIcons().add(icon);
+            stage.setTitle("BlackBoard");
+            stage.setScene(scene);
+            stage.show();
+        }
 
-
-public class Menu extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("menuScreen.fxml"));
-        Scene scene = new Scene(root);
-        root.getStylesheets().add(getClass().getResource("Scene.css").toExternalForm());
-        Image icon = new Image(getClass().getResource("GameIcon.jpeg").toExternalForm());
-        stage.getIcons().add(icon);
-        stage.setTitle("BlackBoard");
-        stage.setScene(scene);
-        stage.show();
+        public static void main(String[] args) {
+            launch(new String[0]);
+        }
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
-}
+
