32421e9f15533722f167a3f70b1ac96adad2889f
Jack-Barsoum <jack.barsoum04@gmail.com>
2024-01-27 20:37:25 +0000

    Added Functionality to Quit Button

--- src/main/java/com/example/boardgame/GameController.java
+++ src/main/java/com/example/boardgame/GameController.java
@@ -1,9 +1,20 @@
 package com.example.boardgame;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class GameController {
 
+    @FXML
+    private Button play;
+
+    @FXML
+    private Button quit;
+
+    @FXML
+    void leave(ActionEvent event) {
+        System.out.println("Quitting Game");
+        System.exit(0);
+    }
 
 }
